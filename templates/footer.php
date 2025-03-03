bb7dacb275ff0efac3fa44b3a5e9108fb6e0318a
Lunire <66011212136@msu.ac.th>
2025-03-03 10:50:12 +0700

    ver.4.2

--- templates/footer.php
+++ templates/footer.php
@@ -10,4 +10,29 @@ if (isset($_SESSION['timestamp'])) {
 
 </body>
 
+        <script>
+            document.addEventListener("DOMContentLoaded", function() {
+            var currentPath = window.location.pathname; // ดึง URL ปัจจุบัน
+            var navLinks = document.querySelectorAll(".nav-link"); // ค้นหาเมนูทั้งหมด
+
+            navLinks.forEach(link => {
+                if (link.getAttribute("href") === currentPath) {
+                    link.classList.add("active");
+                }
+                });
+            });
+
+            function confirmDelete() {
+                return confirm("คุณแน่ใจแล้วใช่ไหมที่จะลบกิจกรรมนี้?");
+            }
+
+            function confirmAlright() {
+                return confirm("คุณแน่ใจแล้วใช่ไหมที่จะกดยอมรับ?");
+            }
+
+            function confirmReject() {
+                return confirm("คุณแน่ใจแล้วใช่ไหมที่จะกดปฏิเสธ?");
+            }
+        </script>
+
 </html>
